5db9ba380539d8473e01e7c3fa54243609d6c908
Gugui80 <manuelodoghertyzufia@gmail.com>
2023-03-13 15:54:44 +0100

    Ejercicios DOM

---
+++ practica8-DesarrolloCliente/js/selectores_atributos.js
@@ -0,0 +1,13 @@
+// Obtenemos <div id="page" class="info data dark" data-number="5"></div>
+let div = document.querySelector("#pagina");
+
+document.write(div.hasAttribute("data-number") + "<br>");  // true (data-number existe)
+document.write(div.hasAttributes() + "<br>");              // true (tiene 3 atributos)
+
+document.write(div.getAttributeNames() + "<br>");          // ["id", "data-number", "class"]
+document.write(div.getAttribute("id") + "<br>");           // "pagina"
+
+div.removeAttribute("id") + "<br>";        
+document.write(div.getAttributeNames() + "<br>"); // class="info data dark" y data-number="5"
+div.setAttribute("id", "pagina2") + "<br>";   
+document.write(div.getAttribute("id") + "<br>"); ; // Añader id="pagina2"
