2d541ab9e7bc48cb53aa90de97e38c8f04dbd259
Gugui80 <manuelodoghertyzufia@gmail.com>
2023-03-23 16:36:17 +0100

    Eventos en el DOM

---
+++ practica8-DesarrolloCliente/js/eventos.js
@@ -0,0 +1,3 @@
+manejadorExterno = evento => {
+    console.log(evento)//Al invocar el método desde HTML con el objeto event, vemos que se almacena: PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+}
